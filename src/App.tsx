36db6db6e9ede4315f3bf4ee4b4122e13e4e1c11
vojtan <vojtech.nadvornik@live.com>
2025-04-08 14:23:05 +0200

    changed language

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,52 @@ interface SearchResult {
 }
 function App() {
   const [searchText, setSearchText] = useState('');
-  const [results, setResults] = useState<Array<SearchResult>>([]); // State to store search results
+  const [results, setResults] = useState<Array<SearchResult>>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signature = "?sv=2024-11-04&ss=bf&srt=co&sp=rx&se=2026-04-08T19:35:57Z&st=2025-04-08T11:35:57Z&spr=https&sig=Bl7RxF73HhCad2OBVXEcSQpEdIDPhwlfpfEBgIjedZo%3D"
+  const blobStorageUrl = "https://stsemanticindex.blob.core.windows.net/data/"
 
   async function search() {
-    const response = await fetch(`/api/search?query=${encodeURIComponent(searchText)}`);
-    const data = await response.json();
-    setResults(data); // Update results state with fetched data
+    setIsLoading(true);
+    try {
+      const response = await fetch(`/api/search?query=${encodeURIComponent(searchText)}`);
+      const data = await response.json();
+      setResults(data); // Update results state with fetched data
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    } finally {
+      setIsLoading(false);
+    }
   }
+
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <input
+        {isLoading ? <div>Načítám</div> : <> <input
           type="text"
           placeholder="Co chcete vyhledat?"
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
         />
-        <button onClick={search}>Search</button>
+          <button onClick={search}>Vyhledat</button>
+        </>}
         {results.length > 0 && (
           <table>
             <thead>
               <tr>
-                <th>ID</th>
-                <th>URL</th>
-                <th>Score</th>
+                <th>Id</th>
+                <th>Url</th>
+                <th>Skóre</th>
               </tr>
             </thead>
             <tbody>
               {results.map((item, index) => (
                 <tr key={index}>
                   <td>{item.id}</td>
-                  <td><a href={item.url} target="_blank" rel="noopener noreferrer">{item.url}</a></td>
+                  <td><a href={`${blobStorageUrl}${item.url}${signature}`} target="_blank" rel="noopener noreferrer">{item.url}</a></td>
                   <td>{item.score}</td>
                 </tr>
               ))}
