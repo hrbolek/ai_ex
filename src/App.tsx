bde5c4684cc85cdc5444253e79a412cec603a6cb
vojtan <vojtech.nadvornik@live.com>
2025-04-08 09:05:35 +0200

    changed to typescript

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input type="text" placeholder="Co chcete vyhledat?" />
+        
+        
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
